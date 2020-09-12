ebd414b8e13ea16fbc1409c9eb76b4e7220edc8e
Freeman <31339365+freemancho1@users.noreply.github.com>
2020-09-12 22:22:57 +0900

    Create 11 Tensorflow GPU Device Test.py

---
+++ 11 Tensorflow GPU Device Test.py
@@ -0,0 +1,15 @@
+import sys
+import tensorflow as tf
+
+from tensorflow import keras
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+print(f'Python version : {sys.version}')
+print(f'Tensorflow version : {tf.__version__}{"-gpu" if tf.config.experimental.list_physical_devices("GPU") else ""}')
+print(f'Keras version : {keras.__version__}')
+
+print(f'\n사용 가능한 GPU Device: {tf.test.gpu_device_name()}')
